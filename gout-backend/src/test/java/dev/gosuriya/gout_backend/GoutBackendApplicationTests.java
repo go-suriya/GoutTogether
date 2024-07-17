e5e3b31a907110cdfcb2896b79ef3fa585bb554b
Pakphom Promsean <pakphom.p@extend-it-resource.com>
2024-07-17 13:53:01 +0700

    Initial Project

---
+++ gout-backend/src/test/java/dev/gosuriya/gout_backend/GoutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gosuriya.gout_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
